a3419cbad6f29f3d9fca9508ead60a79aad52f4c
Chitranjan Gupta <gchitranjan82@gmail.com>
2026-03-18 18:11:48 +0530

    Add files via upload

---
+++ day-17.c
@@ -0,0 +1,47 @@
+/*Problem: Write a program to find the maximum and minimum values present in a given array of integers.
+
+Input:
+- First line: integer n
+- Second line: n integers
+
+Output:
+- Print the maximum and minimum elements
+
+Example:
+Input:
+6
+3 5 1 9 2 8
+
+Output:
+Max: 9
+Min: 1*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int a[100], max, min;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    max = a[0];
+    min = a[0];
+
+    for(i = 1; i < n; i++)
+    {
+        if(a[i] > max)
+            max = a[i];
+
+        if(a[i] < min)
+            min = a[i];
+    }
+
+    printf("Max: %d\n", max);
+    printf("Min: %d", min);
+
+    return 0;
+}
