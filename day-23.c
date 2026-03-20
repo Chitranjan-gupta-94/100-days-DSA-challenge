096a8efd7776e1fa65799908e58ca30fcb06f405
Chitranjan Gupta <gchitranjan82@gmail.com>
2026-03-20 18:18:56 +0530

    Add files via upload

---
+++ day-23.c
@@ -0,0 +1,160 @@
+/*Problem: Merge Two Sorted Linked Lists - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers (first list)
+- Third line: integer m
+- Fourth line: m space-separated integers (second list)
+
+Output:
+- Print the merged linked list elements, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+4
+15 25 35 45
+
+Output:
+10 15 20 25 30 35 40 45 50
+
+Explanation:
+Compare nodes of both lists, append smaller to result, continue until all nodes are merged.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node* createList(int n)
+{
+    struct node *head = NULL, *temp = NULL, *newnode;
+    int x, i;
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+
+        newnode = (struct node*)malloc(sizeof(struct node));
+        newnode->data = x;
+        newnode->next = NULL;
+
+        if(head == NULL)
+        {
+            head = newnode;
+            temp = newnode;
+        }
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+    }
+
+    return head;
+}
+
+struct node* merge(struct node *l1, struct node *l2)
+{
+    struct node *result = NULL, *temp = NULL;
+
+    while(l1 != NULL && l2 != NULL)
+    {
+        struct node *newnode = (struct node*)malloc(sizeof(struct node));
+
+        if(l1->data < l2->data)
+        {
+            newnode->data = l1->data;
+            l1 = l1->next;
+        }
+        else
+        {
+            newnode->data = l2->data;
+            l2 = l2->next;
+        }
+
+        newnode->next = NULL;
+
+        if(result == NULL)
+        {
+            result = newnode;
+            temp = newnode;
+        }
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+    }
+
+    while(l1 != NULL)
+    {
+        struct node *newnode = (struct node*)malloc(sizeof(struct node));
+        newnode->data = l1->data;
+        newnode->next = NULL;
+
+        if(result == NULL)
+        {
+            result = newnode;
+            temp = newnode;
+        }
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+
+        l1 = l1->next;
+    }
+
+    while(l2 != NULL)
+    {
+        struct node *newnode = (struct node*)malloc(sizeof(struct node));
+        newnode->data = l2->data;
+        newnode->next = NULL;
+
+        if(result == NULL)
+        {
+            result = newnode;
+            temp = newnode;
+        }
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+
+        l2 = l2->next;
+    }
+
+    return result;
+}
+
+int main()
+{
+    int n, m;
+    struct node *list1, *list2, *merged, *temp;
+
+    scanf("%d", &n);
+    list1 = createList(n);
+
+    scanf("%d", &m);
+    list2 = createList(m);
+
+    merged = merge(list1, list2);
+
+    temp = merged;
+
+    while(temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
