31b6d79de99a8b98b2dfbd76af083297865c7a71
Chitranjan Gupta <gchitranjan82@gmail.com>
2026-03-27 19:56:42 +0530

    Add files via upload

---
+++ day-44.c
@@ -0,0 +1,107 @@
+/*Problem Statement:
+Perform inorder, preorder, and postorder traversals of a given binary tree.
+
+Input Format:
+- First line contains integer N
+- Second line contains level-order traversal (-1 represents NULL)
+
+Output Format:
+- Print inorder, preorder, and postorder traversals
+
+Example:
+Input:
+7
+1 2 3 4 5 6 7
+
+Output:
+4 2 5 1 6 3 7
+1 2 4 5 3 6 7
+4 5 2 6 7 3 1*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node* createNode(int x)
+{
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = x;
+    newnode->left = NULL;
+    newnode->right = NULL;
+    return newnode;
+}
+
+void inorder(struct node* root)
+{
+    if(root == NULL) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+void preorder(struct node* root)
+{
+    if(root == NULL) return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(struct node* root)
+{
+    if(root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main()
+{
+    int n, i;
+    int a[100];
+    struct node* nodes[100];
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(a[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(nodes[i] != NULL)
+        {
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+
+            if(left < n)
+                nodes[i]->left = nodes[left];
+
+            if(right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    inorder(nodes[0]);
+    printf("\n");
+
+    preorder(nodes[0]);
+    printf("\n");
+
+    postorder(nodes[0]);
+
+    return 0;
+}
